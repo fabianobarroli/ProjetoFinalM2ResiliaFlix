cdd75cbdd8c62ea6c3801f734fa70cd8b68b1b34
vanessacreis <vanessacreis@live.com>
2021-12-28 16:06:25 -0300

    Inclusão da class da Model

--- src/js/models/filmes-models.js
+++ src/js/models/filmes-models.js
@@ -1,21 +1,98 @@
 class Model {
   constructor(valorFilme) {
     this.filme = valorFilme;
+    this.title = "Title";
+    this.year = "Year";
+    this.actors = "Actor";
+    this.director = "Director";
+    this.plot = "Plot";
+    this.genre = "Genre";
+    this.awards = "Awards";
+    this.poster = "Poster";
+    this.ratting = "Ratting";
+    this.language = "Language";
+    this.country = "Country";
+    this.runtime = "Runtime";
   }
 
-  async buscaFilme() {
+  buscaFilme() {
     let chave = "4c45a71f";
-    let url = `http://www.omdbapi.com/?apikey=${chave}&t=${this.filme}`;
-    let reqFilme = await fetch(url).then((response) => response.json());
-    return reqFilme;
+    const requisicao = new XMLHttpRequest();
+    requisicao.open(
+      "GET",
+      `http://www.omdbapi.com/?apikey=${chave}&t=${this.filme}`,
+      false
+    );
+    requisicao.onload = () => {
+      if (requisicao.status == 200) {
+        const response = JSON.parse(requisicao.responseText);
+        console.log(response);
+        this._atualizaFilmes(response);
+      }
+    };
+    requisicao.send();
+  }
 
-    // $.ajax({
-    //   url: `http://www.omdbapi.com/?apikey=4c45a71f&t=${this.filme}`,
-    //   type: "GET",
-    //   success: function (filme) {
-    //     console.log(filme);
-    //     return filme;
-    //   },
-    // });
+  _atualizaFilmes(objeto) {
+    this.title = objeto.Title;
+    this.year = objeto.Year;
+    this.actors = objeto.Actors;
+    this.director = objeto.Director;
+    this.plot = objeto.Plot;
+    this.genre = objeto.Genre;
+    this.awards = objeto.Awards;
+    this.poster = objeto.Poster;
+    this.ratting = objeto.Ratting;
+    this.language = objeto.Language;
+    this.country = objeto.Country;
+    this.runtime = objeto.Runtime;
+  }
+
+  getTitulo() {
+    return this.title;
+  }
+
+  getAno() {
+    return this.year;
+  }
+
+  getAtores() {
+    return this.actors;
+  }
+
+  getDiretor() {
+    return this.director;
+  }
+
+  getPlot() {
+    return this.plot;
+  }
+
+  getGenero() {
+    return this.genre;
+  }
+
+  getPremio() {
+    return this.awards;
+  }
+
+  getPoster() {
+    return this.poster;
+  }
+
+  getRatting() {
+    return this.ratting;
+  }
+
+  getIdioma() {
+    return this.language;
+  }
+
+  getPais() {
+    return this.country;
+  }
+
+  getDuracao() {
+    return this.runtime;
   }
 }
