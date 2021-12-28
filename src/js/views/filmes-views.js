47615d0d229368056f543b38b1861b296b4e7e3a
vanessacreis <vanessacreis@live.com>
2021-12-28 16:07:19 -0300

    Inclusão do método mostraFilme

--- src/js/views/filmes-views.js
+++ src/js/views/filmes-views.js
@@ -1,5 +1,36 @@
 class View {
   mostraFilme(filme) {
-    $("#tituloFilme").text(filme.Title);
+    const poster = filme.getPoster();
+    $("#poster").attr("src", poster);
+
+    const titulo = filme.getTitulo();
+    $("#tituloFilme").text(titulo);
+
+    const ano = filme.getAno();
+    $("#ano").text(ano);
+
+    const atores = filme.getAtores();
+    $("#atores").text(atores);
+
+    const diretor = filme.getDiretor();
+    $("#diretor").text(diretor);
+
+    const genero = filme.getGenero();
+    $("#genero").text(genero);
+
+    const plot = filme.getPlot();
+    $("#plot").text(plot);
+
+    const ratting = filme.getRatting();
+    $("#ratting").text(ratting);
+
+    const idioma = filme.getIdioma();
+    $("#idioma").text(idioma);
+
+    const pais = filme.getCountry();
+    $("#pais").text(pais);
+
+    const duracao = filme.getDuracao();
+    $("#duracao").text(duracao);
   }
 }
