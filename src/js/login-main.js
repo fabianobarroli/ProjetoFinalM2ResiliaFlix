3bbb6f661f6f3a06455e9cee66457d1d26dc0ad7
sashamoon <sarah.alves8@hotmail.com>
2022-01-04 04:36:20 -0300

    Padronização com o "Cadastro" para que os inputs e validações estejam iguais

--- src/js/login-main.js
+++ src/js/login-main.js
@@ -1,6 +1,19 @@
-$("button").click(function (e) {
-  e.preventDefault();
-  const controler = new Controller($("#email").val());
+$("#email").focusout(function (e) {
+    e.preventDefault();
+    const controler = new Controller($("#email").val());
+    controler.validaEmail();
+});
 
-  controler.validaEmail();
+$("#botao-continuar").click(function (e) {
+    e.preventDefault();
+    if(($('#email').val().indexOf("@") > -1) && ($("#exampleInputPassword1").val().length >= 8)){
+        window.location.href = "/src/html/inicial.html"
+    } else {
+        $("#aviso-preenchimento").html(`
+            <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                Preencha todos os campos corretamente!
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        `); 
+    }
 });
--- src/js/view/login-view.js
+++ src/js/view/login-view.js
@@ -1,5 +1,11 @@
 class View {
-  apareceNaTela(texto) {
-    $("#resposta").text(texto);
-  }
+    apareceNaTela(texto) {
+        if(texto == "Email Valido"){
+            $('#resposta').html('Email válido <i class="fas fa-check"></i>');
+            $('#resposta').css({"color": "#04d976"});
+        } else {
+            $('#resposta').html('Email inválido <i class="fas fa-times"></i>');
+            $('#resposta').css({"color": "red"});
+        }
+    }
 }
