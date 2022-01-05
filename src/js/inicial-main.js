0ee156d4b90b9a680f8c7b4d9cf6b9bc4b0c9383
vanessacreis <vanessacreis@live.com>
2022-01-05 16:26:21 -0300

    Alterações no javascript da página inicial

--- src/js/inicial-main.js
+++ src/js/inicial-main.js
@@ -1,116 +1,107 @@
-let controler = new Controller()
-
 function apertaBotao() {
-    let tituloFilme = document.querySelector(".titulo").textContent
-    $(".container").css("display", "none")
-    $(".carousel").css("display", "none")
-    $("#informacaoPagina").css("display", "flex")
-    $("#voltar").css("display", "flex")
-    controler.chamaRequisicao(tituloFilme)
+  let tituloFilme = document.querySelector(".titulo").textContent;
+  let salvaTitulo = () => {
+    localStorage.setItem("tituloFilme", tituloFilme);
+  };
+  salvaTitulo();
+  window.location.href = "/src/html/info.html";
 }
 
 function apertaBotao2() {
-    let tituloFilme = document.querySelector(".titulo2").textContent
-    $(".container").css("display", "none")
-    $(".carousel").css("display", "none")
-    $("#informacaoPagina").css("display", "flex")
-    $("#voltar").css("display", "flex")
-    controler.chamaRequisicao(tituloFilme)
+  let tituloFilme = document.querySelector(".titulo2").textContent;
+  let salvaTitulo = () => {
+    localStorage.setItem("tituloFilme", tituloFilme);
+  };
+  salvaTitulo();
+  window.location.href = "/src/html/info.html";
 }
 
 function apertaBotao3() {
-    let tituloFilme = document.querySelector(".titulo3").textContent
-    $(".container").css("display", "none")
-    $(".carousel").css("display", "none")
-    $("#informacaoPagina").css("display", "flex")
-    $("#voltar").css("display", "flex")
-    controler.chamaRequisicao(tituloFilme)
+  let tituloFilme = document.querySelector(".titulo3").textContent;
+  let salvaTitulo = () => {
+    localStorage.setItem("tituloFilme", tituloFilme);
+  };
+  salvaTitulo();
+  window.location.href = "/src/html/info.html";
 }
 
 function apertaBotao4() {
-    let tituloFilme = document.querySelector(".titulo4").textContent
-    $(".container").css("display", "none")
-    $(".carousel").css("display", "none")
-    $("#informacaoPagina").css("display", "flex")
-    $("#voltar").css("display", "flex")
-    controler.chamaRequisicao(tituloFilme)
+  let tituloFilme = document.querySelector(".titulo4").textContent;
+  let salvaTitulo = () => {
+    localStorage.setItem("tituloFilme", tituloFilme);
+  };
+  salvaTitulo();
+  window.location.href = "/src/html/info.html";
 }
 
 function apertaBotao5() {
-    let tituloFilme = document.querySelector(".titulo5").textContent
-    $(".container").css("display", "none")
-    $(".carousel").css("display", "none")
-    $("#informacaoPagina").css("display", "flex")
-    $("#voltar").css("display", "flex")
-    controler.chamaRequisicao(tituloFilme)
+  let tituloFilme = document.querySelector(".titulo5").textContent;
+  let salvaTitulo = () => {
+    localStorage.setItem("tituloFilme", tituloFilme);
+  };
+  salvaTitulo();
+  window.location.href = "/src/html/info.html";
 }
 
 function apertaBotao6() {
-    let tituloFilme = document.querySelector(".titulo6").textContent
-    $(".container").css("display", "none")
-    $(".carousel").css("display", "none")
-    $("#informacaoPagina").css("display", "flex")
-    $("#voltar").css("display", "flex")
-    controler.chamaRequisicao(tituloFilme)
+  let tituloFilme = document.querySelector(".titulo6").textContent;
+  let salvaTitulo = () => {
+    localStorage.setItem("tituloFilme", tituloFilme);
+  };
+  salvaTitulo();
+  window.location.href = "/src/html/info.html";
 }
 
 function apertaBotao7() {
-    let tituloFilme = document.querySelector(".titulo7").textContent
-    $(".container").css("display", "none")
-    $(".carousel").css("display", "none")
-    $("#informacaoPagina").css("display", "flex")
-    $("#voltar").css("display", "flex")
-    controler.chamaRequisicao(tituloFilme)
+  let tituloFilme = document.querySelector(".titulo7").textContent;
+  let salvaTitulo = () => {
+    localStorage.setItem("tituloFilme", tituloFilme);
+  };
+  salvaTitulo();
+  window.location.href = "/src/html/info.html";
 }
 
 function apertaBotao8() {
-    let tituloFilme = document.querySelector(".titulo8").textContent
-    $(".container").css("display", "none")
-    $(".carousel").css("display", "none")
-    $("#informacaoPagina").css("display", "flex")
-    $("#voltar").css("display", "flex")
-    controler.chamaRequisicao(tituloFilme)
+  let tituloFilme = document.querySelector(".titulo8").textContent;
+  let salvaTitulo = () => {
+    localStorage.setItem("tituloFilme", tituloFilme);
+  };
+  salvaTitulo();
+  window.location.href = "/src/html/info.html";
 }
 
 function apertaBotao9() {
-    let tituloFilme = document.querySelector(".titulo9").textContent
-    $(".container").css("display", "none")
-    $(".carousel").css("display", "none")
-    $("#informacaoPagina").css("display", "flex")
-    $("#voltar").css("display", "flex")
-    controler.chamaRequisicao(tituloFilme)
+  let tituloFilme = document.querySelector(".titulo9").textContent;
+  let salvaTitulo = () => {
+    localStorage.setItem("tituloFilme", tituloFilme);
+  };
+  salvaTitulo();
+  window.location.href = "/src/html/info.html";
 }
 
 function apertaBotao10() {
-    let tituloFilme = document.querySelector(".titulo10").textContent
-    $(".container").css("display", "none")
-    $(".carousel").css("display", "none")
-    $("#informacaoPagina").css("display", "flex")
-    $("#voltar").css("display", "flex")
-    controler.chamaRequisicao(tituloFilme)
+  let tituloFilme = document.querySelector(".titulo10").textContent;
+  let salvaTitulo = () => {
+    localStorage.setItem("tituloFilme", tituloFilme);
+  };
+  salvaTitulo();
+  window.location.href = "/src/html/info.html";
 }
 
 function apertaBotao11() {
-    let tituloFilme = document.querySelector(".titulo11").textContent
-    $(".container").css("display", "none")
-    $(".carousel").css("display", "none")
-    $("#informacaoPagina").css("display", "flex")
-    $("#voltar").css("display", "flex")
-    controler.chamaRequisicao(tituloFilme)
+  let tituloFilme = document.querySelector(".titulo11").textContent;
+  let salvaTitulo = () => {
+    localStorage.setItem("tituloFilme", tituloFilme);
+  };
+  salvaTitulo();
+  window.location.href = "/src/html/info.html";
 }
 
 function apertaBotao12() {
-    let tituloFilme = document.querySelector(".titulo12").textContent
-    $(".container").css("display", "none")
-    $(".carousel").css("display", "none")
-    $("#informacaoPagina").css("display", "flex")
-    $("#voltar").css("display", "flex")
-    controler.chamaRequisicao(tituloFilme)
+  let tituloFilme = document.querySelector(".titulo12").textContent;
+  let salvaTitulo = () => {
+    localStorage.setItem("tituloFilme", tituloFilme);
+  };
+  salvaTitulo();
+  window.location.href = "/src/html/info.html";
 }
-
-$("#voltar").click( () => {
-    $("#voltar").css("display", "none")
-    $(".container").css("display", "flex")
-    $(".carousel").css("display", "flex")
-    $("#informacaoPagina").css("display", "none")
-})
