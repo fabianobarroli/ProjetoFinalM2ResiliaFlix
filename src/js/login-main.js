10caff3f78e83352e2e10f025bcc18b1c7f11379
sashamoon <sarah.alves8@hotmail.com>
2022-01-05 07:01:08 -0300

    Padronização do código após ter feito merge em algumas partes da senha

--- src/js/login-main.js
+++ src/js/login-main.js
@@ -17,3 +17,25 @@ $("#botao-continuar").click(function (e) {
         `); 
     }
 });
+
+$(".btn-enviar").click(function (e) {
+    e.preventDefault();
+    if(($('#email').val().indexOf("@") > -1)){
+        $(".formulario").html(`
+            <div class="alert alert-success" role="alert">
+                <h4 class="alert-heading">Email enviado!</h4>
+                <p>Isso aí, seu e-mail foi identificado e enviamos um link de redefinição de senha, basta clicar nele e escolher uma senha nova! &#10024;</p>
+                <hr>
+                <p class="mb-0">Qualquer dúvida ou problema ligue para o nosso <strong>SAC 0800 7877 0200.</strong></p>
+            </div>
+            <a type="button" class="btn btn-success" href="/src/html/login.html">Ir para página de Login</a>`
+        );
+    } else {
+        $("#aviso-preenchimento").html(`
+            <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                Preencha o campo do email corretamente!
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        `); 
+    }
+});
